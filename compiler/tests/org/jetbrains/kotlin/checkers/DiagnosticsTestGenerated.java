daa7eeef891160a53ce0145b1e0aff9e9cc7c33a
Dmitry Jemerov <yole@jetbrains.com>
2015-12-10 16:12:55 +0100

    remove support for "package.foo.bar" syntax inside files

--- compiler/tests/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
@@ -457,12 +457,6 @@ public class DiagnosticsTestGenerated extends AbstractDiagnosticsTest {
             doTest(fileName);
         }
 
-        @TestMetadata("PackageAsExpression.kt")
-        public void testPackageAsExpression() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/PackageAsExpression.kt");
-            doTest(fileName);
-        }
-
         @TestMetadata("PackageInExpressionPosition.kt")
         public void testPackageInExpressionPosition() throws Exception {
             String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/PackageInExpressionPosition.kt");
@@ -12198,12 +12192,6 @@ public class DiagnosticsTestGenerated extends AbstractDiagnosticsTest {
             @TestMetadata("GenericClassVsPackage.kt")
             public void testGenericClassVsPackage() throws Exception {
                 String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/qualifiedExpression/GenericClassVsPackage.kt");
-                doTest(fileName);
-            }
-
-            @TestMetadata("PackageKeyword.kt")
-            public void testPackageKeyword() throws Exception {
-                String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/qualifiedExpression/PackageKeyword.kt");
                 doTest(fileName);
             }
 
